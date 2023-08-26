d9743a1d78c20b77be94ea91c2a89032400f90a6
105555 <fter2257@gmail.com>
2023-08-26 19:28:28 +0800

    修改路徑

--- vite.config.js
+++ vite.config.js
@@ -25,7 +25,7 @@ function moveOutputPlugin() {
 export default defineConfig({
   // base 的寫法：
   // base: '/Repository 的名稱/'
-  base: '/ViteWebSite/',
+  base: '/Week5_FormDesign_Vite/',
   plugins: [
     liveReload(['./layout/**/*.ejs', './pages/**/*.ejs', './pages/**/*.html']),
     ViteEjsPlugin(),
@@ -33,7 +33,7 @@ export default defineConfig({
   ],
   server: {
     // 啟動 server 時預設開啟的頁面
-    open: 'pages/index.html',
+    open: 'pages/admin.html',
   },
   build: {
     rollupOptions: {
